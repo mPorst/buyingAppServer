b5319ac5d787d8ed31913a343ac4501a11265c73
Moritz Porst <moritz.porst@gmx.de>
2018-09-05 14:35:07 +0200

    moved socket functions to socketHandling.php

--- serverScriptBuyingApp.php
+++ serverScriptBuyingApp.php
@@ -1,60 +1,18 @@
 #!/usr/bin/php
 <?php
-$ip = gethostbyname("palaven.de");
+include_once "mysqlBackend.php";
+include_once "socketHandling.php";
+	
+/* Set IP and Port for sockets */
+$strIPorDOMAIN = "DOMAIN";
+$host = "palaven.de";
 $port = 39978;
 
-if(!($sock = socket_create(AF_INET, SOCK_STREAM, 0)))
-{
-	$errorcode = socket_last_error();
-	$errormsg = socket_strerror($errorcode);
-	die("Could not create socket: [$errorcode] $errormsg \n");
-}
-
-if(!(socket_bind($sock, $ip, $port)))
-{
-	$errorcode = socket_last_error();
-	$errormsg = socket_strerror($errorcode);
-	die("Could not bind socket: [$errorcode] $errormsg \n");
-}
-	echo "Bound socket to IP $ip and port $port \n";
-
-if(!socket_listen($sock, 10))
-{
-	$errorcode = socket_last_error();
-	$errormsg = socket_strerror($errorcode);
-	die("Could not listen on socket: [$errorcode] $errormsg \n");
-}
-	echo "socket listening \n";
-
-	// now start the actual server loop
-	do{
-	echo "Awaiting new client connection \n";
-	$client = socket_accept($sock);
-	     
-	//display information about the client who is connected
-	if(socket_getpeername($client , $address , $port))
-	{
-	    echo "Client $address : $port is now connected to us. \n";
-	}
-	echo "start reading \n";
-
-	if(false === ($msg = socket_read($client, 2048, PHP_NORMAL_READ)))
-	{
-		die("error when reading \n");
-	}
-	echo "read a message: ";
-	echo "$msg \n";
-
-	$message = "This is a test sent from $ip \n";
-
-	if( ! socket_send ( $client , $message , strlen($message) , 0))
-	{
-	    $errorcode = socket_last_error();
-	    $errormsg = socket_strerror($errorcode);
-	     
-	    die("Could not send data: [$errorcode] $errormsg \n");
-	}	
-	echo "sent back message: $message";
-
-	} while (true);
+init_mysqlServer();
+$sock = init_socket($strIPorDOMAIN, $host, $port);
+do{
+	$client = acceptClient($sock);
+	$msg = receiveMessage($client);
+	sendMessage($client, "Hey there");
+} while(true);
 ?>
---
+++ socketHandling.php
@@ -0,0 +1,80 @@
+<?php
+
+function init_socket($strIPorDOMAIN, $host, $port){
+
+	if($strIPorDOMAIN === "IP")
+	{
+		$ip = $host;
+	}
+	else if($strIPorDOMAIN === "DOMAIN")
+	{
+		$ip = gethostbyname($host);
+	}
+	else
+	{
+		die("in serverScriptBuyingApp.php you have to either specify \"IP\" or \"DOMAIN\" \n");
+	}
+
+	/* Create Socket */
+	if(!($sock = socket_create(AF_INET, SOCK_STREAM, 0)))
+	{
+		$errorcode = socket_last_error();
+		$errormsg = socket_strerror($errorcode);
+		die("Could not create socket: [$errorcode] $errormsg \n");
+	}
+
+	/* Bind To Previously Specified IP and Port */
+	if(!(socket_bind($sock, $ip, $port)))
+	{
+
+		$errorcode = socket_last_error();
+		$errormsg = socket_strerror($errorcode);
+		die("Could not bind socket: [$errorcode] $errormsg \n");
+	}
+
+	/* Set Socket To Listen */
+	if(!socket_listen($sock, 10))
+	{
+		$errorcode = socket_last_error();
+		$errormsg = socket_strerror($errorcode);
+		die("Could not listen on socket: [$errorcode] $errormsg \n");
+	}
+	echo "Socket is now listening on IP $ip and port $port \n";
+	return $sock;
+}
+
+function acceptClient($sock){
+	echo "Awaiting new client connection \n";
+	$client = socket_accept($sock);
+	return $client;
+}
+
+function receiveMessage($client){
+	     
+	//display information about the client who is connected
+	if(socket_getpeername($client , $address , $port))
+	{
+	    echo "Client $address : $port is now connected to us. \n";
+	}
+	echo "start reading \n";
+
+	if(false === ($msg = socket_read($client, 2048, PHP_NORMAL_READ)))
+	{
+		die("error when reading \n");
+	}
+	echo "received a message: ";
+	echo "$msg \n";
+	return $msg;
+}
+
+function sendMessage($client, $message){
+	if( ! socket_send ( $client , $message , strlen($message) , 0))
+	{
+	    $errorcode = socket_last_error();
+	    $errormsg = socket_strerror($errorcode);
+	     
+	    die("Could not send data: [$errorcode] $errormsg \n");
+	}	
+	echo "sent message: $message \n";
+}
+?>
